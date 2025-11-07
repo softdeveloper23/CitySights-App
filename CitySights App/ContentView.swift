be3bcb33c2077ee3dbb622645cd4931518f54518
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-07 12:27:21 +0100

    -Change text -Add style to button

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
+        HStack {
+            TextField("What are you looking for?", text: $query)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                // TODO: Implement query
+            } label: {
+                Text("GO")
+                    .padding(.horizontal)
+                    .padding(.vertical, 10)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
