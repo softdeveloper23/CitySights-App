93bd240aba08e383c692c8e51a643a30eee689cd
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-15 20:02:38 +0100

    [Feature] Adds map markers for businesses
    Adds map markers to the map view representing the businesses fetched from the data model. This enhances the user experience by visually displaying business locations on the map.

--- CitySights App/MapView.swift
+++ CitySights App/MapView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @Environment(BusinessModel.self) var model
+    
     var body: some View {
-        Map()
+        Map() {
+            ForEach(model.businesses, id: \.id) { business in
+                Marker(business.name ?? "Restaurant", coordinate: CLLocationCoordinate2D(latitude: business.coordinates?.latitude ?? 0, longitude: business.coordinates?.longitude ?? 0))
+            }
+        }
     }
 }
 
 #Preview {
     MapView()
+        .environment(BusinessModel())
 }
