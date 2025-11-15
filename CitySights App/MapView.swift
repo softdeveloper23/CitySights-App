a7d2fba83a1966f8f8d7d43464e2b1228a473588
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-15 20:49:38 +0100

    [Feature] Add map marker selection
    Implement a feature that allows the user to select a map marker.
    Updates the selected business in the model when a marker is selected.

--- CitySights App/MapView.swift
+++ CitySights App/MapView.swift
@@ -10,11 +10,24 @@ import MapKit
 
 struct MapView: View {
     @Environment(BusinessModel.self) var model
-    
+    @State var selectedBusinessId: String?
     var body: some View {
-        Map() {
+        Map(selection: $selectedBusinessId) {
             ForEach(model.businesses, id: \.id) { business in
-                Marker(business.name ?? "Restaurant", coordinate: CLLocationCoordinate2D(latitude: business.coordinates?.latitude ?? 0, longitude: business.coordinates?.longitude ?? 0))
+                Marker(business.name ?? "Restaurant",
+                       coordinate: CLLocationCoordinate2D(latitude: business.coordinates?.latitude ?? 0, longitude: business.coordinates?.longitude ?? 0))
+                .tag(business.id ?? "None")
+            }
+        }
+        .onChange(of: selectedBusinessId) { oldValue, newValue in
+            // Find the business which matches this id
+            let business = model.businesses.first { business in
+                business.id == selectedBusinessId
+            }
+            
+            // If the business is found, set it as the selected one
+            if let business = business {
+                model.selectedBusiness = business
             }
         }
     }
