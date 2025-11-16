b1648eff8b4d13b03f679ccc4a3cdf6274f689a1
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-16 20:22:35 +0100

    [Feature] Adds core location functionality
    Implements core location to get the user's current location.
    It requests location permission, retrieves coordinates,
    and triggers a business search when location is available.
    Also handles location updates and authorization changes.

--- CitySights App/BusinessModel.swift
+++ CitySights App/BusinessModel.swift
@@ -9,12 +9,19 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-@Observable class BusinessModel {
+@Observable class BusinessModel: NSObject, CLLocationManagerDelegate {
     var businesses = [Business]()
     var query: String = ""
     var selectedBusiness: Business?
     var service = DataService()
     var locationManager = CLLocationManager()
+    var currentUserLocation: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.delegate = self
+    }
     
     func getBusinesses() {
         Task {
@@ -25,9 +32,31 @@ import CoreLocation
     func getUserLocation() {
         // Check if we have permission
         if locationManager.authorizationStatus == .authorizedWhenInUse {
+            currentUserLocation = nil
             locationManager.requestLocation()
         } else {
             locationManager.requestWhenInUseAuthorization()
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print(error)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // Detect if user allowed, then request location
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            currentUserLocation = nil
+            manager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentUserLocation = locations.last?.coordinate
+        if currentUserLocation != nil {
+            // Call business search
+            getBusinesses()
+        }
+        manager.stopUpdatingLocation()
     }
 }
