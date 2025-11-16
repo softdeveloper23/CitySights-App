d27142916f7799f19e21be9a6cae0456109ec4c1
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-16 18:59:40 +0100

    [Feature] Adds paging dots to onboarding
    Improves the onboarding experience by adding paging
    dots to indicate the current page and allow users to
    easily navigate between onboarding screens.
    
    Updates the background color to change dynamically based
    on the selected page.

--- CitySights App/Onboarding/OnboardingView.swift
+++ CitySights App/Onboarding/OnboardingView.swift
@@ -12,26 +12,47 @@ struct OnboardingView: View {
     @State var selectedViewIndex = 0
     
     var body: some View {
-        TabView(selection: $selectedViewIndex) {
-            OnboardingViewDetails(bgColor: Color(red: 111/255, green: 154/255, blue: 189/255),
-                                  headline: "Welcome to City Sights",
-                                  subHeadline: "City Sights helps you find the best of the city!") {
-                withAnimation {
-                    selectedViewIndex = 1
+        ZStack {
+            if selectedViewIndex == 0 {
+                Color(red: 111/255, green: 154/255, blue: 189/255)
+            } else {
+                Color(red: 139/255, green: 166/255, blue: 65/255)
+            }
+            TabView(selection: $selectedViewIndex) {
+                OnboardingViewDetails(bgColor: Color(red: 111/255, green: 154/255, blue: 189/255),
+                                      headline: "Welcome to City Sights",
+                                      subHeadline: "City Sights helps you find the best of the city!") {
+                    withAnimation {
+                        selectedViewIndex = 1
+                    }
                 }
+                                      .tag(0)
+                                      .ignoresSafeArea()
+                
+                OnboardingViewDetails(bgColor: Color(red: 139/255, green: 166/255, blue: 65/255),
+                                      headline: "Discover Your City",
+                                      subHeadline: "We'll show you the best restaurants, venues, and more based on your location.") {
+                    dismiss()
+                }
+                                      .tag(1)
+                                      .ignoresSafeArea()
             }
-                                  .tag(0)
-                                  .ignoresSafeArea()
-            
-            OnboardingViewDetails(bgColor: Color(red: 139/255, green: 166/255, blue: 65/255),
-                                  headline: "Discover Your City",
-                                  subHeadline: "We'll show you the best restaurants, venues, and more based on your location.") {
-                dismiss()
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            VStack {
+                Spacer()
+                HStack(spacing: 16) {
+                    Spacer()
+                    Circle()
+                        .frame(width: 10)
+                        .foregroundStyle(selectedViewIndex == 0 ? .white : .gray)
+                    Circle()
+                        .frame(width: 10)
+                        .foregroundStyle(selectedViewIndex == 1 ? .white : .gray)
+                    Spacer()
+                }
+                .padding(.bottom, 220)
             }
-                                  .tag(1)
-                                  .ignoresSafeArea()
         }
-        .tabViewStyle(.page)
         .ignoresSafeArea()
     }
 }
